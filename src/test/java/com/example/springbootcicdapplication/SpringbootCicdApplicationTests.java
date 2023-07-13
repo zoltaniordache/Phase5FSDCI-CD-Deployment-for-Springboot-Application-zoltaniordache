fd941f8916f6097ba2e7103f3fb97987f5d6d698
zoltaniordache <zotlan.iordache@gmail.com>
2023-07-13 12:15:09 +0000

    First Commit

---
+++ src/test/java/com/example/springbootcicdapplication/SpringbootCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcicdapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
