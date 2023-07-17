990cd46f45ff7c1a24f24c38f27287fac1ccc6d3
zoltaniordache <zotlan.iordache@gmail.com>
2023-07-17 14:35:58 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Welcome to ReactJs created by Zoltan Iordache!
         </a>
       </header>
     </div>
